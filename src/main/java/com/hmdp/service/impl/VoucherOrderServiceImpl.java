a6d0f7af4ef4023d415a29660440df73f7989ded
calmkin <490363906@qq.com>
2023-03-18 16:27:42 +0800

    解决了一人一单问题(事务+悲观锁)

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -10,6 +10,7 @@ import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIDWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -60,23 +61,61 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存数量不足");
         }
 
+
+
+        Long Usrid = UserHolder.getUser().getId();
+
+        //根据用户的id进行加锁
+        synchronized (Usrid.toString().intern())     //intern方法解决tostring底层的缺陷
+        {
+            //return createOrder(voucherId);      //这种方法会导致事务失效
+
+            //获取代理对象
+            IVoucherOrderService orderService =(IVoucherOrderService) AopContext.currentProxy();
+            return orderService.createOrder(voucherId);
+        }
+
+    }
+
+    /**
+     * 查询用户是否已经下过单，如果没有的话，就创建订单
+     * 涉及查询和插入操作，需要开启事务
+     * @param voucherId
+     * @return
+     */
+    @Transactional
+    public Result createOrder(Long voucherId)
+    {
+        //用户id可以通过线程变量获取
+        Long Usrid = UserHolder.getUser().getId();
+
+        //解决一人一单问题：先查询用户是否已经下过单，如果下过单，直接返回错误信息
+        LambdaQueryWrapper<VoucherOrder> voucherOrderLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        voucherOrderLambdaQueryWrapper.eq(VoucherOrder::getUserId,Usrid);
+        int count = this.count(voucherOrderLambdaQueryWrapper);
+        //如果用户已经下过单了，那么直接返回错误
+        if(count>0)
+        {
+            return Result.fail("该用户已下过单！");
+        }
+
         //库存减一
-        voucher.setStock(StockNum-1);
-        LambdaQueryWrapper<SeckillVoucher> lqw = new LambdaQueryWrapper<>();
-        lqw.eq(SeckillVoucher::getVoucherId,voucherId);
-
         //乐观锁解决超卖问题
-        lqw.gt(SeckillVoucher::getStock,0);
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1") // set stock = stock - 1
+                .eq("voucher_id", voucherId).gt("stock", 0) // where id = ? and stock > 0
+                .update();
 
-        seckillVoucherService.update(voucher,lqw);
+        if(!success)
+        {
+            return Result.fail("库存不足！");
+        }
 
         //创建订单（设置用户id、优惠券id，订单id）
         VoucherOrder order = new VoucherOrder();
         order.setVoucherId(voucherId);
-
-        Long Usrid = UserHolder.getUser().getId();
         order.setUserId(Usrid);
-
+        //为订单生成全局唯一ID
         long orderId = redisIDWorker.nextId("shop");
         order.setId(orderId);
 
@@ -86,4 +125,5 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         this.save(order);
         return Result.ok(orderId);
     }
+
 }
